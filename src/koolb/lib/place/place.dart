ba365921e8a94eab97a805732e01e9012d9b748b
blgcatAPCS <blgcat20@apcs.fitus.edu.vn>
2022-11-03 16:18:09 +0700

    update renter and place

--- src/koolb/lib/place/place.dart
+++ src/koolb/lib/place/place.dart
@@ -1,7 +1,14 @@
 class Place {
   //Contructor
-  String name;
+  String _name;
+
+  String get name => _name;
+
+  set name(String name) {
+    _name = name;
+  }
 //add kinh độ vĩ độ
 
   //Function
+  Place(this._name);
 }
--- src/koolb/lib/renter/renter.dart
+++ src/koolb/lib/renter/renter.dart
@@ -1,7 +1,8 @@
-class renter {
+class Renter {
   // Constructor
   String name, username, email, fb;
   DateTime dob;
 
   //Function
+  Renter(this.name, this.username, this.email, this.fb, this.dob);
 }
