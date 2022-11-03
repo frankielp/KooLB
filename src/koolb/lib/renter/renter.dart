44c573a319bd589236d84ec99cd6c92618a0ac3c
juicydoggo <frankiecomrade@gmail.com>
2022-11-03 16:32:13 +0700

    Update renter.dart

--- src/koolb/lib/renter/renter.dart
+++ src/koolb/lib/renter/renter.dart
@@ -1,33 +1,33 @@
 class Renter {
   // Constructor
-  String _name, _username, _email, _fb;
-  DateTime _dob;
+  String name, username, email, fb;
+  DateTime dob;
 
-  String get name => _name;
-  set name(String name) {
-    _name = name;
-  }
+  // String get name => _name;
+  // set name(String name) {
+  //   _name = name;
+  // }
 
-  String get username => _username;
-  set username(String username) {
-    _username = username;
-  }
+  // String get username => _username;
+  // set username(String username) {
+  //   _username = username;
+  // }
 
-  String get email => _email;
-  set email(String email) {
-    _email = email;
-  }
+  // String get email => _email;
+  // set email(String email) {
+  //   _email = email;
+  // }
 
-  String get fb => _fb;
-  set fb(String fb) {
-    _fb = fb;
-  }
+  // String get fb => _fb;
+  // set fb(String fb) {
+  //   _fb = fb;
+  // }
 
-  String get name => _name;
-  set name(String name) {
-    _name = name;
-  }
+  // String get name => _name;
+  // set name(String name) {
+  //   _name = name;
+  // }
 
   //Function
-  Renter(this._name, this._username, this._email, this._fb, this._dob);
+  Renter(this.name, this.username, this.email, this.fb, this.dob);
 }
