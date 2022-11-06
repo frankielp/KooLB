a951f9d653dc0f7ab040521751bb608076b7946d
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2022-11-06 19:45:01 +0700

    get current location of user

--- src/koolb/lib/ui/renter/pages/google_maps.dart
+++ src/koolb/lib/ui/renter/pages/google_maps.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart' as geolocator;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(const RenterMaps());
 
@@ -13,10 +17,29 @@ class RenterMaps extends StatefulWidget {
 class _RenterMapsState extends State<RenterMaps> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  // final LatLng _center = const LatLng(45.521563, -122.677433);
+  // static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  // static const LatLng destinationLocation = LatLng(37.33429383, -122.06600055);
+
+  late LatLng currentLocation;
+
+  void getCurrentLocation() async {
+    geolocator.Position position =
+        await geolocator.Geolocator.getCurrentPosition(
+            desiredAccuracy: geolocator.LocationAccuracy.high);
+    setState(() {
+      currentLocation = LatLng(position.latitude, position.longitude);
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
   }
 
   @override
@@ -27,13 +50,21 @@ class _RenterMapsState extends State<RenterMaps> {
           title: const Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+        body: currentLocation == null
+            ? const Center(child: Text('Loading'))
+            : GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: currentLocation,
+                  zoom: 20.5,
+                ),
+                markers: {
+                  Marker(
+                    markerId: const MarkerId("currentLocation"),
+                    position: currentLocation,
+                  ),
+                },
+              ),
       ),
     );
   }
