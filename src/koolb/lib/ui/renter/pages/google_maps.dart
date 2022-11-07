311582f4184a44fa20728f0166756473c80f057c
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2022-11-07 16:38:45 +0700

    add multiple markers

--- src/koolb/lib/ui/renter/pages/google_maps.dart
+++ src/koolb/lib/ui/renter/pages/google_maps.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart' as geolocator;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
@@ -22,6 +23,8 @@ class _RenterMapsState extends State<RenterMaps> {
   // static const LatLng destinationLocation = LatLng(37.33429383, -122.06600055);
 
   late LatLng currentLocation;
+  List<Marker> marker = [];
+  Set<Marker> markers = new Set();
 
   void getCurrentLocation() async {
     geolocator.Position position =
@@ -36,9 +39,29 @@ class _RenterMapsState extends State<RenterMaps> {
     mapController = controller;
   }
 
+  void getHotelLocation() {
+    markers.add(const Marker(
+      markerId: MarkerId("1"),
+      position: LatLng(37.33500926, -122.03272188),
+      infoWindow: InfoWindow(
+        title: 'Hotel 1',
+      ),
+    ));
+    markers.add(const Marker(
+      markerId: MarkerId("2"),
+      position: LatLng(27.7137735, 85.315626),
+      infoWindow: InfoWindow(
+        title: 'Hotel 2',
+      ),
+    ));
+  }
+
   @override
   void initState() {
     getCurrentLocation();
+    getHotelLocation();
+    // marker.addAll(list);
+
     super.initState();
   }
 
@@ -58,14 +81,28 @@ class _RenterMapsState extends State<RenterMaps> {
                   target: currentLocation,
                   zoom: 20.5,
                 ),
-                markers: {
-                  Marker(
-                    markerId: const MarkerId("currentLocation"),
-                    position: currentLocation,
-                  ),
-                },
-              ),
+                markers: getmarkers()),
       ),
     );
+  }
+
+  Set<Marker> getmarkers() {
+    //markers to place on map
+    setState(() {
+      getHotelLocation();
+
+      markers.add(
+        Marker(
+          markerId: const MarkerId("currentLocation"),
+          position: currentLocation,
+          infoWindow: const InfoWindow(
+            title: 'You are here',
+          ),
+        ),
+      );
+      //add more markers here
+    });
+
+    return markers;
   }
 }
