96eaea0b634b9414bba473202efe13fa850ba118
Fuyu Linh <nhnlinh20@apcs.fitus.edu.vn>
2022-11-07 17:08:56 +0700

    Add constructor

--- src/koolb/lib/host/host.dart
+++ src/koolb/lib/host/host.dart
@@ -4,11 +4,13 @@ import 'package:koolb/util/reservation.dart';
 class Host {
   //Constructor
   String name, email, fb;
+  String password, country, language, currency;
+  DateTime DOB;
   DateTime date;
   List<Place> listing;
   List<Reservation> reservation;
 
   //Function
-  Host(this.name, this.email, this.fb, this.date, this.listing,
-      this.reservation);
+  Host(this.name, this.email, this.fb, this.password, this.country, this.DOB,
+      this.language, this.currency, this.date, this.listing, this.reservation);
 }
