b7a440809378e26807831af7735a039b24d747d1
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2022-11-27 20:44:42 +0700

    Update home_page.dart

--- src/koolb/lib/ui/renter/pages/home_page.dart
+++ src/koolb/lib/ui/renter/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:koolb/accommodation/accommodation.dart';
+import 'package:koolb/accommodation/category.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,5 +12,20 @@ class HomePage extends StatelessWidget {
         child: Text('Home Page')
       ),
     );
+  }
+
+  Set<Accommodation> filterResult(Set<Accommodation> accommodation,
+      List<Category> requirement) {
+    accommodation.retainWhere((element) =>
+        filterRequirement(element, requirement) == true);
+    return accommodation;
+  }
+
+  bool filterRequirement(Accommodation accommodation,
+      List<Category> requirement) {
+    for (Category categories in requirement) {
+      if (!accommodation.category.contains(categories)) return false;
+    }
+    return true;
   }
 }
