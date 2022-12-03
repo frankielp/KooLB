4479f7a9cccd026341ed36fe1ade136249693ccb
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2022-12-03 15:48:21 +0700

    merge setting

--- src/koolb/lib/main.dart
+++ src/koolb/lib/main.dart
@@ -38,6 +38,8 @@ import 'package:koolb/ui/renter/r_navigationbar.dart';
 import 'firebase_options.dart';
 import 'package:koolb/ui/sign_in_screen.dart' as SignInPage;
 
+import 'ui/renter/pages/home_page.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -90,11 +92,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       // body: SplashScreen(),
-      body: SettingPage(),
+      body: RenterPagesNavigation(),
     );
   }
 }
