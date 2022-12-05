936b3df9753c6eed92cc4efe8e3b17c4d0514640
Fuyu Linh <nhnlinh20@apcs.fitus.edu.vn>
2022-12-05 12:42:45 +0700

    commit

--- src/koolb/lib/ui/renter/pages/chat_page.dart
+++ src/koolb/lib/ui/renter/pages/chat_page.dart
@@ -20,10 +20,29 @@ class _ChatPageState extends State<chatPage> {
               child: Padding(
                 padding: EdgeInsets.only(left: 16, right: 16, top: 10),
                 child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(
+                        Icons.forum,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                      Icon(
+                        Icons.notifications,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                    ]),
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(left: 16, right: 16, top: 14),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text(
-                      "Chat Box",
+                    const Text(
+                      "CHAT BOX",
                       style:
                           TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
                     ),
@@ -39,7 +58,7 @@ class _ChatPageState extends State<chatPage> {
                         children: <Widget>[
                           Icon(
                             Icons.add,
-                            color: Colors.cyan,
+                            color: Colors.black,
                             size: 20,
                           ),
                           SizedBox(
@@ -54,6 +73,26 @@ class _ChatPageState extends State<chatPage> {
                       ),
                     )
                   ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.grey.shade600),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade600,
+                    size: 20,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.grey.shade100)),
                 ),
               ),
             ),
