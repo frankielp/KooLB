b96b4120d569ba1b4538f5aa23a2fb5c7e7cb905
Fuyu Linh <nhnlinh20@apcs.fitus.edu.vn>
2022-12-30 21:40:39 +0700

    commit

--- src/koolb/lib/ui/host/pages/insight_page.dart
+++ src/koolb/lib/ui/host/pages/insight_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class InsightPage extends StatelessWidget {
+class InsightPage extends StatefulWidget {
   const InsightPage({super.key});
 
   @override
+  State<InsightPage> createState() => HostInsightPage();
+}
+
+class HostInsightPage extends State<InsightPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Insight Page')
-      ),
-    );
+    return Scaffold();
   }
 }
