6f0bf51fa5e771b0d22c30a94b2bb3c19b95df70
giakhanh2711 <53686667+giakhanh2711@users.noreply.github.com>
2023-01-01 13:27:53 +0700

    connect-facebook

--- src/koolb/lib/main.dart
+++ src/koolb/lib/main.dart
@@ -99,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ChatPage(),
+      body: SplashScreen(),
     );
   }
 }
--- src/koolb/windows/flutter/generated_plugins.cmake
+++ src/koolb/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_webview_auth
+  flutter_secure_storage_windows
   geolocator_windows
   pasteboard
   url_launcher_windows
