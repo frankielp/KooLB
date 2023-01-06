cdbe8ff34695d0bc8033327e79c773040e4bd84e
blgcatAPCS <blgcat20@apcs.fitus.edu.vn>
2023-01-06 18:55:02 +0700

    test run

--- src/koolb/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/koolb/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,14 @@ import Foundation
 
 import cloud_firestore
 import desktop_webview_auth
+import device_info_plus
+import facebook_auth_desktop
+import firebase_analytics
 import firebase_auth
 import firebase_core
+import firebase_database
+import firebase_storage
+import flutter_secure_storage_macos
 import geolocator_apple
 import location
 import pasteboard
@@ -22,9 +28,11 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
